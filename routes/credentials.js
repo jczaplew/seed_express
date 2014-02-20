491c735a8a63e845a8a43c6635d225c400677300
John J Czaplewski <jczaplew@gmail.com>
2014-02-20 10:42:35 -0600

    Added support for MySQL and Postgres

---
+++ routes/credentials.js
@@ -0,0 +1,8 @@
+exports.mysql = {
+  host     : 'localhost',
+  user     : 'user',
+  password : 'password',
+  database : 'database'
+}
+
+exports.postgres = "postgres://username:password@host:port/database";
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,40 @@
 var http = require('http'),
     fs = require('fs'),
-    async = require('async');
+    async = require('async'),
+    pg = require('pg'),
+    mysql = require('mysql'),
+    credentials = require('./credentials');
+
+/* MySQL Placeholder
+var connection = mysql.createConnection(credentials.mysql);
+connection.connect(function(error) {
+  if (error) {
+    throw error;
+  } 
+});
+
+// Query syntax
+connection.query(sql, params, function(error, result) {
+  if (error) {
+    throw error;
+  }
+  response.json(result);
+});
+
+*/
+
+/* Postgres placeholder
+var client = new pg.Client(credentials.postgres);
+
+// Connect to postgres
+client.connect();
+
+// Query syntax
+client.query(sql, params , function(err, result) {
+  res.json(result.rows);
+});
+
+*/
 
 // Route for root
 exports.root = function(req, res) {
